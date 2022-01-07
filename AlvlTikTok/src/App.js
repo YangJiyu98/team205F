5705b349ff9631207e8b8af3ef700bb3769dd7b5
YangJiyu98 <yang.jiyu.jy@gmail.com>
2022-01-08 04:38:27 +0800

    add ability to navigate questions/show answer

--- AlvlTikTok/src/App.js
+++ AlvlTikTok/src/App.js
@@ -1,27 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import db from './Firebase/firebase';
-import { doc, onSnapshot, collection, query, getDocs} from "firebase/firestore";
+import { doc, onSnapshot, collection, query, getDocs } from "firebase/firestore";
 import { Paper, IconButton } from "@mui/material";
 import CheckBoxOutlinedIcon from "@mui/icons-material/CheckBoxOutlined";
+import { pink } from '@mui/material/colors';
 import CloseIcon from "@mui/icons-material/Close";
-
-// import Store from "./Store";
-// import Read from "./Read";
-// import Add from "./Add";
-
-
-// function MyComponent() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <button onClick={() => setCount(count + 1)}>+</button>
-//       <button onClick={() => setCount(count - 1)}>-</button>
-//       count: {count}
-//     </>
-//   );
-// }
+import DoneIcon from '@mui/icons-material/Done';
 
 function ImageFrame(props) {
   const { imgSrc } = props;
@@ -41,64 +26,67 @@ function TickOrCrossButtons(props) {
 
     <div>
       <IconButton>
-        <CheckBoxOutlinedIcon />
+        <DoneIcon color="success" fontSize="large" bac onClick={() => {
+          props.setIndex(props.index + 1);
+          props.setShowAnswer(false)
+        }
+        } />
       </IconButton>
       <button>
-        <CloseIcon />
+        <CloseIcon sx={{ color: pink[500] }} fontSize="large" onClick={() => {
+          props.setIndex(props.index + 1);
+          props.setShowAnswer(false)
+        }
+        } />
       </button>
     </div>
   );
 }
 
 function ShowAnsButton(props) {
+  const onClick = () => props.setShowAnswer(true);
+
   return (
     <div>
-      <button>Show me the answer plzzzzzz</button>
+      {props.showAnswer ? <ImageFrame imgSrc={props.answer} /> : null}
+      <button onClick={onClick}>Show me the answer plzzzzzz </button>
     </div>
   );
 }
 
 
 function App() {
-  const [question, setQuestion] = useState([])
-  // const fetchQuestion = async () => {
-  //   const response = db.collection("test");
-  //   const data = await response.get();
-  //   data.docs.forEach(item => {
-  //     setQuestion([...question, item.data()])
-  //   })
-  // }
-  // useEffect(() => {
-  //   fetchQuestion();
-  // }, [])
-    useEffect(() => {
-      const q = query(collection(db, "test"));
-      getDocs(q)
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+  const [categories, setCategories] = useState([])
+  const [index, setIndex] = useState(0)
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  useEffect(() => {
+    const q = query(collection(db, "test"));
+    getDocs(q)
       .then((docs) => {
         docs.forEach((doc) => {
           // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
+          const qnObj = doc.data();
+          console.log(doc.id, " => ", qnObj.category);
+          setQuestions(questions => [...questions, qnObj.question])
+          setAnswers(answers => [...answers, qnObj.answer])
+          setCategories(categories => [...categories, qnObj.category])
+          console.log(questions);
         });
       })
-    }, [])
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <ImageFrame imgSrc="https://www.examsolutions.net/wp-content/uploads/2019/06/edx-al-maths-p1-june-2018-q6.jpg" />
-        <TickOrCrossButtons />
-        <ShowAnsButton />
-        {/* <Add /> */}
-        {
-          question && question.map(question => {
-            return (
-              <div className="blog-container">
-                <h4>{question.question}</h4>
-                <p>{question.answer}</p>
-              </div>
+        {/* <ImageFrame imgSrc="https://www.examsolutions.net/wp-content/uploads/2019/06/edx-al-maths-p1-june-2018-q6.jpg" /> */}
+        <p>{index + 1}) {categories[index]} </p>
+        <ImageFrame imgSrc={questions[index]} />
+        <ShowAnsButton showAnswer={showAnswer} setShowAnswer={setShowAnswer} answer={answers[index]} />
 
-            )
-          })
-        }
+        <TickOrCrossButtons setShowAnswer={setShowAnswer} index={index} setIndex={setIndex} />
+
       </header>
     </div>
 
